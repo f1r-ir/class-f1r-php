f3b988f16ddf0202674fbf024355a0d58b3349eb
f1r company <96688882+f1r-ir@users.noreply.github.com>
2022-01-02 10:47:12 +0330

    Update new-link.php

--- test-code/new-link.php
+++ test-code/new-link.php
@@ -3,12 +3,13 @@ include_once 'class.php';
 use F1r_php as shourtlink;
 $url = @$_GET['url'];
 $name = @$_GET['name'];
+$token = @$_GET['token'];
 if (isset($url) && !empty($url)){
   if (!isset($name) or empty($name)){
     $name = "rand";
   }
 }
-$result = shourtlink::creat_link($url,$name);
+$result = shourtlink::creat_link($url,$name,$token);
 if (gettype($result) == "array"){
   echo 'sucsfull'.PHP_EOL."name : {$result['name']}" . PHP_EOL . "sourt_link : {$result['link']}" . PHP_EOL . "status_link : {$result['status']}";
 } else {
